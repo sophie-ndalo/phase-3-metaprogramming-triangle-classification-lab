c804e934b207d3d92e97a858e5729afa146b7ee8
sophia <sophiendalo@gmail.com>
2023-02-14 17:22:37 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @a, @b, @c = a, b, c
+    validate_triangle
+  end
+  
+  def kind
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  private
+  
+  def validate_triangle
+    if @a <= 0 || @b <= 0 || @c <= 0 || (@a + @b <= @c) || (@b + @c <= @a) || (@a + @c <= @b)
+      raise TriangleError, "invalid triangle"
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
